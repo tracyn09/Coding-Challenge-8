f47576222475ce328aa1874a0aef18093e2ed1d5
tracyn09 <tracyn0099@gmail.com>
2025-02-11 15:30:30 -0500

    Task 2–Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -5,3 +5,11 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 }
 //Test Case
 calculateSalary(5000, 500, 0.1)
+
+//Task 2
+function calculateDiscount(price, discountRate) {
+    let finalPrice = price - (price * discountRate)
+    console.log(`Final Price: $${finalPrice}`)
+}
+//Test Case
+calculateDiscount(100, 0.2)
